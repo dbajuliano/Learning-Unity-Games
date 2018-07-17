43f40733db4cb5fe6e234ba6cfa1d0141b267f9e
Juliano tech <julianotech@hotmail.com>
2018-07-17 21:30:00 +0100

    Using GDD and flowing throw all scenes

--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -7,11 +7,12 @@ public class LevelManager : MonoBehaviour
 	public void LoadLevel (string name)
 	{
 		Debug.Log ("Level load request for: " + name);
+		Application.LoadLevel (name);
 	}
 	
 	public void QuitRequest ()
 	{
 		Debug.Log ("I want to quit!"); 
+		Application.Quit();
 	}
-
 }
